07d02a3286c6b9f31a7d8b2de12ae4eb3ef3dcc4
kiwithecode <wowcelodion@gmail.com>
2024-06-03 16:14:44 -0500

    fix rss 3

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,20 +1,21 @@
 import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
 
-export async function GET(context) {
-  const blog = await getCollection('blog');
-  return rss({
-    title: 'Buzz’s Blog',
-    description: 'A humble Astronaut’s guide to the stars',
-    site: context.site,
-    items: blog.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      customData: post.data.customData,
-      // Compute RSS link from post `slug`
-      // This example assumes all posts are rendered as `/blog/[slug]` routes
-      link: `/blog/${post.slug}/`,
-    })),
-  });
-}
+export const get = () => rss({
+  title: 'Mi Blog',
+  description: 'Descripción de mi blog',
+  site: 'https://mi-sitio.com',
+  items: [
+    {
+      link: '/post-1',
+      title: 'Título del Post 1',
+      description: 'Descripción del Post 1', // Agregar si es necesario
+      pubDate: new Date('2023-01-01').toUTCString(),
+    },
+    {
+      link: '/post-2',
+      title: 'Título del Post 2',
+      description: 'Descripción del Post 2', // Agregar si es necesario
+      pubDate: new Date('2023-01-02').toUTCString(),
+    },
+  ],
+});
