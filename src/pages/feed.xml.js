9550b2dd9bc290a7d714d7a2817e22acb6f7a78b
kiwithecode <wowcelodion@gmail.com>
2024-06-03 16:12:43 -0500

    fix rss 2

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -4,17 +4,17 @@ import { getCollection } from 'astro:content';
 export async function GET(context) {
   const blog = await getCollection('blog');
   return rss({
-    title: 'Brutal Blog',
-    description: 'Brutal is a theme for Astro',
-    stylesheet: false,
+    title: 'Buzz’s Blog',
+    description: 'A humble Astronaut’s guide to the stars',
     site: context.site,
     items: blog.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
+      customData: post.data.customData,
+      // Compute RSS link from post `slug`
+      // This example assumes all posts are rendered as `/blog/[slug]` routes
       link: `/blog/${post.slug}/`,
     })),
-    customData: '<language>en-us</language>',
-    canonicalUrl: 'https://brutal.elian.codes',
   });
 }
